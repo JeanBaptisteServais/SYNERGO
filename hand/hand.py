eb4abbae8abfa9902d87203ba6440a36fd959c6c
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2020-01-13 19:13:15 +0100

    Add files via upload

---
+++ hand/hand.py
@@ -0,0 +1,254 @@
+import cv2
+import math
+import time
+
+import imutils
+import numpy as np
+#import tensorflow as tf
+from matplotlib import pyplot as plt
+from numpy import expand_dims, squeeze
+
+from scipy.spatial import distance as dist
+
+
+from palm_analyse import palm_analyse
+from no_finger_found import no_finger_found
+from reorganize_finger import reorganize_finger
+from reorganize_phax_position import reorganize_phax_position
+from hand_location import hand_location
+from skeletton import hand_skelettor
+from hand_mask import skin_detector, hand_treatment, make_bitwise
+from sign import sign
+
+def save(crop, C):
+    cv2.imwrite(r"C:\Users\jeanbaptiste\Desktop\hand_picture\a" + str(C) + ".jpg", crop)
+    C += 1
+    return C
+
+def load_inference_graph(path_to_ckpt):
+    # load frozen tensorflow model into memory
+    detection_graph = tf.Graph()
+
+    with detection_graph.as_default():
+        od_graph_def = tf.compat.v1.GraphDef()
+        with tf.io.gfile.GFile(path_to_ckpt, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        sess = tf.compat.v1.Session(graph=detection_graph)
+    return detection_graph, sess
+
+
+
+def detect_objects(image_np, detection_graph, sess):
+
+
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    image_np_expanded = expand_dims(image_np, axis=0)
+
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+
+    (boxes, scores) = sess.run([detection_boxes, detection_scores],
+                               feed_dict={image_tensor: image_np_expanded})
+
+    return squeeze(boxes), squeeze(scores)
+
+
+
+def hands_detections(scores, boxes, frame):
+    """We detect hands, by position we define if it are to left or to right.
+    Recup it for history if we have impossible detections"""
+
+    height, width = frame.shape[:2]
+    detections = [(int(boxes[i][1] * width), int(boxes[i][0] * height),
+                   int(boxes[i][3] * width), int(boxes[i][2] * height))
+                   for i in range(2) if (scores[i] > 0.10)]
+
+    return detections
+
+
+
+
+
+def treat_skeletton_points(skeletton, position, finger, proba, rectangle, crop):
+
+
+    x, y, w, h = rectangle
+
+    palm_center =  position[0][0]
+
+    palm = [[position[5][0], position[9][0], position[13][0],
+             position[17][0], position[0][1]],
+
+            [position[5][0], position[9][0], position[13][0],
+             position[17][0], position[0][1]]]
+
+    #attribuate finger's to their initial detection
+    thumb = position[1:4]
+    index = position[5:8]
+    major = position[9:12]
+    annular = position[13:16]
+    auricular = position[17:20]
+
+    miss_points = no_finger_found(finger, thumb, index, major, annular, auricular)
+
+    #location of the thumb
+    hand_localised = hand_location(thumb, index, major, annular, auricular, crop)
+
+    #area of the palm
+    fingers_direction = palm_analyse(hand_localised, palm_center, palm, rectangle, crop,
+                                     thumb, index, major, annular, auricular)
+
+
+
+    #delete false points finger detection
+    finger_sorted, fingers_orientation = reorganize_phax_position(thumb, index, major, annular,
+                                             auricular, crop, fingers_direction)
+
+    #reorganize finger's position
+    thumb, index, major, annular, auricular =\
+    reorganize_finger(hand_localised, crop, miss_points, finger_sorted, fingers_orientation)
+
+    #reattribuate points
+    thumb = finger_sorted[0]
+    index = finger_sorted[1]
+    major = finger_sorted[2]
+    annular = finger_sorted[3]
+    auricular = finger_sorted[4]
+
+
+
+    #thumb_analyse(palm_center, thumb, index, crop)
+    #index_analyse(thumb, index, major, crop)
+
+    #major_analyse(major, palm_center, rectangle, crop)
+
+    #annular_analyse(annular, palm_center, rectangle, crop)
+    #auricular_analyse(auricular, palm_center, rectangle, crop)
+
+
+
+
+    sign(thumb, index)
+
+
+
+
+
+
+
+
+
+
+C = 0
+
+def hand(frame, detection_graph, sess, head_box):
+    global C
+    frame_copy = frame.copy()
+
+    #head_hand_distance_possibility(head_box, frame)
+
+    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    boxes, scores = detect_objects(frameRGB, detection_graph, sess)
+
+    detections = hands_detections(scores, boxes, frame)
+
+    for nb, hand in enumerate(detections):
+
+        skinYCrCb, crop, copy = skin_detector(hand, frame, frame_copy)
+        copy = crop.copy()
+
+        contours = hand_treatment(skinYCrCb, crop)
+        copy, rectangle = make_bitwise(contours, copy)
+
+        C = save(copy, C)
+
+
+        protoFile = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handa\models\pose_deploy.prototxt"
+        weightsFile = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handa\models\pose_iter_102000.caffemodel"
+
+        #points, position, finger = hand_skelettor(copy, protoFile, weightsFile)
+        #treat_skeletton_points(points, position, finger, rectangle, crop)
+
+
+    #cv2.imshow("crop_convex", frame_copy)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+
+
+    IM = 27
+
+
+    image = r"C:\Users\jeanbaptiste\Desktop\hand_picture\a{}.jpg".format(str(IM))
+    #image = r"C:\Users\jeanbaptiste\Desktop\hand_picture\{}.jpg".format(str(IM))
+
+    img = cv2.imread(image)
+    copy_img = img.copy()
+
+    protoFile = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handa\models\pose_deploy.prototxt"
+    weightsFile = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handa\models\pose_iter_102000.caffemodel"
+
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    contours = [sorted(cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0],
+                                        key=cv2.contourArea)][0]
+
+
+
+    copy = img.copy()
+    rectangle = cv2.boundingRect(contours[-1])
+
+
+
+    points, position, finger, proba = hand_skelettor(copy_img, protoFile, weightsFile)
+    treat_skeletton_points(points, position, finger, proba, rectangle, img)
+
+
+
+#FIXED
+    #mains avec tous les doigts 5 doigts visibles semblent ok
+
+
+
+
+
+#TODO
+
+    
+    
+    #image 5 un pec change major et le doigt dapres
+    #changement de perspective doigt plus long -> penché vers torse et versa #531
+    #angle entre debut doigt et fin ex 259 doigt coté face
+    #angle 261
+    #pouce via index via majeur ect ex pouce rond index -> 261
+
+
+#FUNCTION
+    #rangement des pts du doigt -> 77
+    #egalité réglé 1
+    #delete phax 17; 25; 27
+    #reorganisation doigt 23 (pts theorique non respecté)
+    #finger remove 27
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hand/reorganize_phax_position.py
@@ -0,0 +1,226 @@
+import cv2
+from scipy.spatial import distance as dist
+
+
+def no_detection_orientatation(fingers_orientation):
+    """Des fois y'a des egalités du coup on définit le sens du doigt par apport aux autres"""
+
+
+    positions = [i[1] for i in fingers_orientation]
+    print("sens des doigts semblent etre a : ", positions)
+    pos = ["gauche", "droite", "haut", "bas"]
+
+    if positions != []:
+        indexage = [positions.count(i) for i in pos]
+        pos = pos[indexage.index(max(indexage))]
+
+        for i in fingers_orientation:
+            if i[1] == "egal":
+                i[1] = pos
+
+    return fingers_orientation
+
+
+def fingers_tratment(fingers):
+    """We recuperate all fingers without doublon and None detection (0,0)"""
+    return list(set([j for i in fingers for j in i if i != (0, 0)]))
+
+def sorted_data(data, position):
+
+    if position == "gauche":
+        data_sorted = sorted(data, key=lambda tup: tup[0], reverse=True)
+    elif position == "droite":
+        data_sorted = sorted(data, key=lambda tup: tup[0])
+    elif position == "haut":
+        data_sorted = sorted(data, key=lambda tup: tup[1], reverse=True) 
+    elif position == "bas":
+        data_sorted = sorted(data, key=lambda tup: tup[1])
+    else: data_sorted = data
+
+    return data_sorted
+
+
+
+def delete_finger(sorted_fingers, crop):
+    aa = crop.copy()
+    print("delete_finger")
+
+
+    to_remove = []
+
+    for i in range(len(sorted_fingers)):
+
+        same_points_localisation = 0
+        copy_delete = crop.copy()
+
+        if i < len(sorted_fingers) - 1:
+
+            #Draw
+            [cv2.circle(copy_delete, j, 2, (255, 0, 0), 2) for j in sorted_fingers[i]]
+            [cv2.circle(copy_delete, j, 2, (0, 0, 255), 2) for j in sorted_fingers[i + 1]]
+            print(sorted_fingers[i], "\n", sorted_fingers[i + 1])
+
+            #Distance
+            for j in sorted_fingers[i]:
+                for k in sorted_fingers[i + 1]:
+                    if abs(j[0] - k[0]) <= 15 and\
+                       abs(j[1] - k[1]) <= 15:
+                        same_points_localisation += 1
+
+            #5 identics localisations
+            if same_points_localisation >= 5:
+
+
+                #Choice delete finger
+                finger_length1 = len(sorted_fingers[i])
+                finger_length2 = len(sorted_fingers[i + 1])
+
+                print(finger_length1, finger_length2)
+
+                if finger_length1 > finger_length2:
+                    to_remove.append(i + 1)
+                    [cv2.circle(copy_delete, j, 2, (0, 0, 0), 2) for j in sorted_fingers[i + 1]]
+
+                elif finger_length2 > finger_length1 or finger_length2 == finger_length1:
+                    to_remove.append(i)
+                    [cv2.circle(copy_delete, j, 2, (0, 0, 0), 2) for j in sorted_fingers[i]]
+
+                print("finger removed")
+
+
+            cv2.imshow("copy_delete", copy_delete)
+            cv2.waitKey(0)
+
+
+    for i in to_remove:
+        sorted_fingers.remove(sorted_fingers[i])
+
+    for i in sorted_fingers:
+        print(i)
+
+    print("")
+
+
+    return sorted_fingers
+
+
+
+def delete_phax(sorted_fingers, copy):
+    #Parcours all points of a finger.
+    #If two points are more than 40 (space beetween finger's are ~ 15-10 and there are 3 spaces)
+        #recuperate the first point of the finger and the 2 points fingers (sort before)
+        #we delete the one who's the far from the original
+
+    print("delete_phax")
+    print(sorted_fingers)
+    for fingers in sorted_fingers:
+
+        for finger1 in fingers:
+            if len(fingers) == 2:
+                for finger2 in fingers:
+
+                    if dist.euclidean(finger1, finger2) >= 40:
+
+                        legnth_to_origin1 = dist.euclidean(fingers[0], finger2)
+                        legnth_to_origin2 = dist.euclidean(fingers[0], finger1)
+
+                        if legnth_to_origin1 > legnth_to_origin2:
+                            fingers.remove(finger2)
+                            cv2.circle(copy, finger2, 2, (0, 0, 255), 2)
+                            cv2.imshow("DELETED", copy)
+                            cv2.waitKey(0)
+     
+                        if legnth_to_origin2 > legnth_to_origin1:
+                            fingers.remove(finger1)
+                            cv2.circle(copy, finger1, 2, (0, 0, 255), 2)
+                            cv2.imshow("DELETED", copy)
+                            cv2.waitKey(0)
+
+            elif len(fingers) > 2:
+                for finger2 in fingers[1:-1]:
+
+                    if dist.euclidean(finger1, finger2) >= 40:
+
+                        legnth_to_origin1 = dist.euclidean(fingers[0], finger2)
+                        legnth_to_origin2 = dist.euclidean(fingers[0], finger1)
+
+                        if legnth_to_origin1 > legnth_to_origin2:
+                            fingers.remove(finger2)
+                            cv2.circle(copy, finger2, 2, (0, 0, 255), 2)
+                            cv2.imshow("DELETED", copy)
+                            cv2.waitKey(0)
+     
+                        if legnth_to_origin2 > legnth_to_origin1:
+                            fingers.remove(finger1)
+                            cv2.circle(copy, finger1, 2, (0, 0, 255), 2)
+                            cv2.imshow("DELETED", copy)
+                            cv2.waitKey(0)
+
+
+    print("")
+    return sorted_fingers
+
+
+
+
+def reorganize_phax_position(thumb, index, major, annular, auricular, crop, fingers_direction):
+    """Sometimes we have false detection 2 times the same finger,
+    one point detected on an another point.
+    So we remove them"""
+
+    print("reorganize_phax_position")
+
+    copy = crop.copy()
+
+    #all points to delete (0, 0)
+    fingers = [thumb, index, major, annular, auricular]
+    fingers = [fingers_tratment(fingers[nb]) for nb in range(5)]
+
+
+    #We recuperate finger's with their orientation to top left right or bot.
+    fingers_orientation = [([i] + [k[2]]) for i in fingers
+                           for j in i for k in fingers_direction if j == k[1]]
+
+    fingers_orientation = no_detection_orientatation(fingers_orientation)
+
+
+
+    #Now we can sort them for example finger to top so we take max to min y points.
+    #Sort data in function of orientation
+    sorted_fingers = [sorted_data(i[0], i[1]) for i in fingers_orientation]
+
+
+    #remove None detections
+    for fingers in sorted_fingers:
+        for finger in fingers:
+            if finger == (0, 0):
+                fingers.remove(finger)
+
+    #Delete phax
+    sorted_fingers = delete_phax(sorted_fingers, copy)
+
+
+    for fingers in sorted_fingers:
+        for i in fingers:
+            cv2.circle(copy, i, 2, (0, 255, 0), 2)
+
+    cv2.imshow("DELETE", copy)
+    cv2.waitKey(0)
+
+
+    #Delete finger
+    sorted_fingers = delete_finger(sorted_fingers, crop)
+
+
+
+    for i in sorted_fingers:
+        print(i)
+    print("")
+
+    for i in fingers_orientation:
+        print(i)
+
+    print("")
+
+    return sorted_fingers, fingers_orientation
+
