fb5e4c1c6c606c5528ecb8070ab5fd3f30ff23a1
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2020-01-15 22:50:52 +0100

    Add files via upload

---
+++ hand/identify_fingers.py
@@ -0,0 +1,165 @@
+import cv2
+from scipy.spatial import distance as dist
+
+
+
+def thumb_to_next_finger():
+    
+
+def identify_fingers(thumb, fingers, crop, rectangle, direction, axis):
+
+    print("")
+    print("IDENTIFY FINGERS")
+
+    #Ratio aspect
+    print("Box de la main est de: ", rectangle)
+    #Need the thumb for detect the next first finger
+    print(thumb)
+    #Our finger's
+    print(fingers)
+    #Direction for the location for the next first finger
+    print(direction, axis)
+
+    copy = crop.copy()
+
+    [cv2.circle(copy, i, 2, (0, 0, 0), 2) for i in thumb[0]]
+    [cv2.circle(copy, j, 2, (0, 0, 255), 2) for i in fingers for j in i[0]]
+
+    #Add None then replace by () My first lambda function
+    fingers += [None for i in range(4 - len(fingers))]
+    points = [(lambda x: () if x == None else x[0][-1])(i) for i in fingers]
+
+
+    #Draw functions
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+
+    draw = lambda picture, text, pts:\
+        cv2.putText(copy, text, pts, font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+
+    draw_line = lambda picture, pts1, pts2:\
+        cv2.line(picture, pts1, pts2, (0, 255, 0), 1)
+
+
+    #detect thumb
+    cv2.circle(copy, thumb[0][-1], 2, (255, 255, 255), 2)
+    draw(copy, "P", thumb[0][-1])
+
+    fing = ["I", "M", "An", "a"]
+    x, y, w, h = rectangle
+
+    #Choose the ratio length
+    if direction in ("droite", "gauche"):
+        area = "width"
+    elif direction in ("bas", "haut"):
+        area = "height"
+
+
+
+    for i in points:
+        if i != ():
+            print(dist.euclidean(i, thumb[0][-1]))
+
+    thumb_index = dist.euclidean(points[0], thumb[0][-1])
+    print(thumb_index, (w,h))
+
+    if area == "width" and thumb_index < w * 0.574 or\
+       area == "height" and thumb_index < w * 0.574:
+        draw(copy, fing[0], points[0])
+        draw_line(copy, points[0], thumb[0][-1])
+        fing.remove(fing[0])
+
+    elif area == "width" and w * 0.775 > thumb_index > w * 0.574 or\
+         area == "height" and w * 0.775 > thumb_index > w * 0.574:
+        draw(copy, fing[1], points[0])
+        draw_line(copy, points[0], thumb[0][-1])
+        for i in range(2):
+            fing.remove(fing[0])
+
+    elif 130 > thumb_index > 105:
+        draw(copy, fing[2], points[0])
+        draw_line(copy, points[0], thumb[0][-1])
+        fing.remove(fing[0])
+        for i in range(3):
+            fing.remove(fing[0])
+
+    elif thumb_index > 130:
+        draw(copy, fing[3], points[0])
+        draw_line(copy, points[0], thumb[0][-1])
+        for i in range(4):
+            fing.remove(fing[0])
+
+
+    cv2.imshow("thumb_next_finger", copy)
+    cv2.waitKey(0)
+
+
+    print("")
+
+    for i in range(len(points)):
+
+        print(fing)
+
+        if i < len(points) - 1 and points[i] != () and points[i + 1] != ():
+
+
+            a = dist.euclidean(points[i], points[i + 1])
+            print(a, (w, h))
+
+
+            #One point after
+            if a < w * 0.295 and area == "width" or\
+               a < w * 0.295 and area == "height":
+                print("Moins 35")
+                draw_line(copy, points[i], points[i + 1])
+                draw(copy, fing[0], points[i + 1])
+                fing.remove(fing[0])
+
+
+            elif len(fing) == 1:
+                print("reste plus qu'un doigt")
+                draw_line(copy, points[i], points[i + 1])
+                draw(copy, fing[0], points[i + 1])
+                fing.remove(fing[0])
+
+
+            elif (w * 0.295) * 2 > a > w * 0.295:
+                print("1 doigt apres")
+                draw_line(copy, points[i], points[i + 1])
+                draw(copy, fing[1], points[i + 1])
+                for i in range(2):
+                    fing.remove(fing[0])
+
+
+            elif (w * 0.295) * 3 > a > (w * 0.295) * 2:
+                print("2 doigts apres")
+                draw_line(copy, points[i], points[i + 1])
+                draw(copy, fing[2], points[i + 1])
+                for i in range(3):
+                    fing.remove(fing[0])
+                fing.remove(fing[0])
+
+            elif (w * 0.295) * 4 > a > (w * 0.295) * 3:
+                print("3 doigts apres")
+                draw_line(copy, points[i], points[i + 1])
+                draw(copy, fing[3], points[i + 1])
+                for i in range(4):
+                    fing.remove(fing[0])
+
+
+            elif a > (w * 0.295) * 4:
+                print("ici ecart supp a * 4")
+
+
+
+            cv2.imshow("thumb_next_finger", copy)
+            cv2.waitKey(0)
+            print("")
+
+
+
+
+
+    if len(fing) > 0: print("manque des doigts :", fing)
+
+
+
--- hand/reorganize_finger.py
+++ hand/reorganize_finger.py
@@ -4,149 +4,122 @@ from scipy.spatial import distance as dist
 def sort_points(fingers, val, to_reverse):
 
     #On recupere le premier point et son axe
-    value = [i[0][0][val] for i in fingers]
+    value = [i[0][0] for i in fingers if i[0] != []]
 
     #Sort point
-    value = sorted(value, reverse=to_reverse)
+    value = sorted(value, key=lambda tup: tup[val], reverse=to_reverse)
 
     #Si on a un points qui match avec nos points sorted on append
-    sorted_points = []
-    for v in value:
-        for i in fingers:
-            if i[0][0][val] == v:
-                sorted_points.append(i)
+    sorted_points = [i for v in value for i in fingers if i[0] != [] and i[0][0] == v]
 
     return sorted_points
 
 
 
-def search_index(thumb, fingers):
+def search_fingers(thumb, fingers, search):
+
+    """
+    si le pouce est a droite alors on cherche nos points par gauche
+    si le pouce est en haut alors on cherche nos points par ordre decroissant par le bas
+    """
 
     print("pouce situé a: ", thumb[1])
 
-    #si le pouce est a droite alors on cherche nos points par gauche
-    if thumb[1] == "droite":
-        search_finger = "gauche"
-    elif thumb[1] == "gauche":
-        search_finger = "droite"
+    #Localisation thumb and area of finger's (i maybe think not other localisation)
+    if thumb[1]   == "droite":    search_finger = "gauche"
+    elif thumb[1] == "gauche":    search_finger = "droite"
+    elif thumb[1] == "haut":      search_finger = "bas"
+    elif thumb[1] == "bas":       search_finger = "haut"
 
-    #si le pouce est en haut alors on cherche nos points par ordre decroissant par le bas
-    elif thumb[1] == "haut":
-        search_finger = "bas"
-    elif thumb[1] == "bas":
-        search_finger = "haut"
-
-    print("recherche par :", search_finger)
+    print("recherche par :", search_finger, "sur axe :", search)
 
     thumb = thumb[0][-1]
+    #u = unkonw
+    #Pouce a gauche recherche de haut en bas
+    if search_finger   == "gauche" and search == "y":     sorted_points = sort_points(fingers, 1, False)
+    #Pouce a gauche recherche de droite a gauche
+    elif search_finger == "gauche" and search == "x":     sorted_points = sort_points(fingers, 0, True)
+    elif search_finger == "gauche" and search == "u":     sorted_points = sort_points(fingers, 0, True)
 
-    #recherche: par hauteur (axe y)
-    print("if probleme et ce qui arrivera c qu'il y a une egalité et faut trancher par x")
-    if search_finger == "gauche" or search_finger == "droite":
-        sorted_points = sort_points(fingers, 1, False)
-
-    #gauche
-    if search_finger == "bas" and thumb[1] == "gauche" or\
-        search_finger == "haut" and thumb[1] == "gauche":
-        sorted_points = sort_points(fingers, 0, True)
-
-    #droite 
-    if search_finger == "bas" and thumb[1] == "droite" or\
-        search_finger == "bas" and thumb[1] == "droite":
-        sorted_points = sort_points(fingers, 0, False)
+    #Pouce a gauche recherche de haut en bas
+    elif search_finger == "droite" and search == "y":     sorted_points = sort_points(fingers, 1, False)
+    #Pouce a gauche recherche de droite a gauche
+    elif search_finger == "droite" and search == "x":     sorted_points = sort_points(fingers, 0, False)
+    elif search_finger == "droite" and search == "u":     sorted_points = sort_points(fingers, 1, False)
 
 
-    for i in sorted_points:
-        print(i)
+    #To verify so make an error
 
-    return sorted_points
-
-
+    elif a == "haut" and search == "y":     sorted_points = sort_points(fingers, 1, True)
+    elif a == "haut" and search == "x":     sorted_points = sort_points(fingers, 0, True)
 
 
-def reorganize_finger(hand_localisation, crop, miss_points,
-                      finger_sorted, fingers_orientation):
+    elif a == "bas" and search == "y":      sorted_points = sort_points(fingers, 1, False)
+    elif a == "bas" and search == "x":      sorted_points = sort_points(fingers, 0, False)
+
+    return sorted_points, search_finger, search
+
+
+
+def reorganize_finger(crop, miss_points, finger_sorted, fingers_orientation):
     """Sometime finger's are detected in a different order like thumb annular index...
     so we sort finger from the thumb and replace them      like thumb index ... annular"""
 
     copy = crop.copy()
 
 
-    print("reorganize_finger")
+    print("REORGANIZE FINGER")
 
     #Verification du pouce
-    if miss_points == 0:
-        print("PROBLEME NO POUCE")
+    if miss_points == 0:    print("PROBLEME NO POUCE")
 
 
+    print(finger_sorted)
+    print(fingers_orientation)
+    
+    #Mélange des doigts triés et de leur orientation
+    fingers = [[i, j[1]] for i, j in zip(finger_sorted, fingers_orientation)]
+
+    thumb = fingers[0]
+    fingers = fingers[1:]
+
+    #Recuperate x and y first point
+    axis = [ [i[0][0][0] for i in fingers if i[0] != []], [i[0][0][1] for i in fingers if i[0] != []] ]
+
+    #Make F(k)|Ki+1 - Ki| = Diff beetween each points = space beetween finger
+    to_list = lambda liste: [abs(liste[i] - liste[i + 1]) for i in range(len(liste))
+                                                          if i < len(liste) - 1]
+
+    axis_x = to_list(axis[0])
+    axis_y = to_list(axis[1])
+
+    print("espace entre doigts sur X: ", axis_x)
+    print("espace entre doigts sur Y: ", axis_y)
+
+    #Recuperate the max spacement
+    space = lambda listeX, listeY: "x" if sum(listeX) > sum(listeY) else\
+                                   "y" if sum(listeX) < sum(listeY) else\
+                                   "u"
+
+    search = space(axis_x, axis_y)
+    print("les espace sur", search)
+
+    #Sorted points from spacement
+    sorted_points, direction, axis = search_fingers(thumb, fingers, search)
 
 
-    #Verification tous les doigts
-    miss = False
-    for i in finger_sorted:
-        if i == []:
-            miss = True
-
-
-    if miss is True:
-        print("manque doigts...................")
-
-        print(finger_sorted)
-        print(fingers_orientation)
-
-        thumb = fingers[0]
-        fingers = fingers[1:]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    else:
-
-        #on mélange les points du doigt + l'orientation
-        fingers = [[i, j[1]] for i, j in zip(finger_sorted, fingers_orientation)]
-
-        thumb = fingers[0]
-        fingers = fingers[1:]
-
-        sorted_points = search_index(thumb, fingers)
-
-        [cv2.circle(copy, i, 2, (0, 0, 0), 2) for i in thumb[0]]
-        for i in sorted_points:
+    #Display
+    [cv2.circle(copy, i, 2, (0, 0, 0), 2) for i in thumb[0]]
+    for i in sorted_points:
+        if i != []:
             for j in i[0]:
                 cv2.circle(copy, j, 2, (0, 255, 255), 2)
-
-            cv2.imshow("thumb", copy)
-            cv2.waitKey(0)
-
-        for i in thumb:
-            print(i)
-
-        for i in fingers:
-            print(i)
+        cv2.imshow("points", copy)
+        cv2.waitKey(0)
 
 
 
-
-
-##        thumb = thumb
-##        index = sorted_points[0]
-##        major = sorted_points[1]
-##        annular = sorted_points[2]
-##        auricular = sorted_points[3]
-
-    print("")
-
-    return thumb, index, major, annular, auricular
+    return thumb, sorted_points, direction, axis
 
 
 
